5d78a8a016f88e45a161e394bced5945db3d43d0
Olivier Croquette <ocroquette@free.fr>
2013-03-19 23:04:19 +0100

    Map now show the UBahn and SBahn stations. The names are not displayed though.

--- tools/mapsforge/generate-map.sh
+++ tools/mapsforge/generate-map.sh
@@ -5,7 +5,6 @@ minlon=11.4
 maxlat=48.3
 maxlon=11.8
 
-export PATH=$PATH:/Volumes/DD500/ocroquette/bin/osmosis-0.42/bin
 if [ ! -f oberbayern-latest.osm.pbf ] ; then
 # wget http://download.geofabrik.de/osm/europe/germany/bayern/oberbayern.osm.bz2 || exit 1
 wget http://download.geofabrik.de/europe/germany/bayern/oberbayern-latest.osm.pbf || exit 1
@@ -15,9 +14,19 @@ if [ ! -f munich.osm ] ; then
 time ( osmosis --rb oberbayern-latest.osm.pbf --bb left=$minlon bottom=$minlat right=$maxlon top=$maxlat --wx munich.osm ) || exit 1
 fi
 
-export JAVACMD_OPTIONS=-Xmx2G
+TMPFILE=`mktemp -t merged`
+
 time osmosis \
   --rx file=munich.osm  \
+  --rx railway-stations.osm \
+  --merge \
+  --wx "$TMPFILE"  || exit 1
+
+
+
+export JAVACMD_OPTIONS=-Xmx2G
+time osmosis \
+  --rx file="$TMPFILE"  \
   --mapfile-writer      \
   file=munich.map       \
   map-start-position=48.132453,11.543895  \
