a973affd61920510a7ce2e18971c73fd6d48192f
larshuth <it@huth.app>
2023-03-14 22:07:08 +0100

    Changed events and routes for 2023

--- config/users/olivier/events/create.sh
+++ config/users/olivier/events/create.sh
@@ -4,8 +4,8 @@ set -e
  
 DATE=date
 FORMAT="%Y-%m-%d"
-start=`$DATE +$FORMAT -d "2022-05-09"`
-end=`$DATE +$FORMAT -d "2022-09-12"`
+start=`$DATE +$FORMAT -d "2023-05-01"`
+end=`$DATE +$FORMAT -d "2023-09-11"`
 now=$start
 while [[ "$now" < "$end" ]] ; do
   now=`$DATE +$FORMAT -d "$now + 1 week"`
